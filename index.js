b3f7c9c017b858295ff5fbde965e10194742375c
A.S.D.Vinay <vinayasd@MacBook-Pro.local>
2017-05-04 22:27:19 +0530

    lnow

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 var PouchDB = require('pouchdb');
 
-var db = new PouchDB('kittens');
+var localDB = new PouchDB('localDB');
 
-PouchDB.debug.enable('*');
+var remoteDB = new PouchDB('http://localhost:5984/myremotedb');
 
-var doc = {
-    "_id": "1",
-    "name": "vinay"
-};
-
-//db.put(doc);
-
-//db.get('1').then(function(doc){
-//    console.log(doc);
-//});
-
-db.get('1').then(function (doc) {
-  doc.name = 'arun';
-  return db.put(doc);
-}).then(function () {
-  return db.get('1');
-}).then(function (doc) {
-  console.log(doc);
+var syncHandler = localDB.sync(remoteDB, {
+  live: true,
+  retry: true
+}).on('change', function (change) {
+  // yo, something changed!
+}).on('paused', function (info) {
+  // replication was paused, usually because of a lost connection
+}).on('active', function (info) {
+  // replication was resumed
+}).on('error', function (err) {
+  // totally unhandled error (shouldn't happen)
 });
+
+syncHandler.on('complete', function (info) {
+  // replication was canceled!
+});
+
+syncHandler.cancel();
